79aa595c27570327a085ad19eb2357d3c2ac888e
Tomáš Korenko <radixxko@gmail.com>
2024-03-13 11:07:01 +0100

    Version 1.1.0

--- src/hash.ts
+++ src/hash.ts
@@ -3,9 +3,9 @@ import { objectStringify } from '@liqd-js/fast-object-hash';
 
 export { objectStringify };
 
-export default function objectHash( obj: any, options: { alg?: 'sha1' | 'sha256', format?: 'hex' | 'base64' | 'base64url' | 'binary', sort?: boolean } = {}): string
+export default function objectHash( obj: any, options: { alg?: 'sha1' | 'sha256', format?: 'hex' | 'base64' | 'base64url' | 'binary', sortArrays?: boolean } = {}): string
 {
-    const { alg = 'sha256', format = 'hex', sort = false } = options;
+    const { alg = 'sha256', format = 'hex', sortArrays = false } = options;
 
-    return crypto.createHash( alg ).update( objectStringify( obj, sort, new Set() )).digest( format );
+    return crypto.createHash( alg ).update( objectStringify( obj, { sortArrays, ignoreUndefinedProperties: true })).digest( format );
 }
